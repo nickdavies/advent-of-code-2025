9e97e7f1d7d4a53d0a443d237ef1dadfeb650ce2
Nick Davies <nick.davies@reddit.com>
2025-12-05 12:29:04 -0700

    Added part 2 solution
    I have not yet submitted this so it may have some bugs

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -21,8 +21,6 @@ pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::
         parse_input(LineSplitter, ParseFromStr, sec2).context("failed to parse input")?;
 
     ranges.sort();
-    println!("{ranges:?}");
-    println!("{ids:?}");
 
     let mut out = 0;
     for id in ids {
@@ -37,8 +35,38 @@ pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::
     Ok(Some(out))
 }
 
-pub fn part_two(_input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::Error> {
-    Ok(None)
+pub fn part_two(input: &str, _run_type: RunType) -> Result<Option<u64>, anyhow::Error> {
+    let (sec1, _) = input.split_once("\n\n").context("failed to split input")?;
+
+    let mut ranges: Vec<(u64, u64)> = parse_input(
+        LineSplitter,
+        ParseTuple2(ParseFromStr, ParseFromStr, "-"),
+        sec1,
+    )
+    .context("failed to parse input")?;
+    ranges.sort();
+
+    let mut out = 0;
+    let mut current = None;
+    for (start, end) in ranges {
+        if let Some((current_start, current_end)) = &mut current {
+            if start <= *current_end {
+                *current_end = std::cmp::max(end, *current_end);
+            } else {
+                let range_size = *current_end - *current_start + 1;
+                out += range_size;
+                current = Some((start, end));
+            }
+        } else {
+            current = Some((start, end));
+        }
+    }
+
+    if let Some((current_start, current_end)) = current {
+        out += current_end - current_start + 1;
+    }
+
+    Ok(Some(out))
 }
 
 #[cfg(test)]
@@ -57,9 +85,19 @@ mod tests_day_5 {
 
     #[test]
     fn test_part_two() -> anyhow::Result<()> {
-        let expected = None;
+        let expected = Some(14);
         let input = &advent_of_code::template::read_file_part("examples", DAY, 2);
         assert!(expected.is_none() || !input.is_empty(), "example 2 empty!");
+        let result = part_two(input, RunType::Example)?;
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two_2() -> anyhow::Result<()> {
+        let expected = Some(14);
+        let input = &advent_of_code::template::read_file_part("examples", DAY, 3);
+        assert!(expected.is_none() || !input.is_empty(), "example 3 empty!");
         let result = part_two(input, RunType::Example)?;
         assert_eq!(result, expected);
         Ok(())
