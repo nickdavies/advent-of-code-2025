c75e2ca72407d869fdd792281a24515ee0e3b328
Nick Davies <nick.davies@reddit.com>
2025-12-05 11:03:18 -0700

    Added day 5 part 1 solution

---
+++ src/bin/05.rs
@@ -0,0 +1,67 @@
+#![allow(unused_imports)]
+advent_of_code::solution!(5);
+
+use advent_of_code::template::RunType;
+use anyhow::{Context, Result, anyhow};
+use aoc_lib::parse::preamble::*;
+use itertools::{EitherOrBoth, Itertools};
+use std::str::FromStr;
+
+pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::Error> {
+    let (sec1, sec2) = input.split_once("\n\n").context("failed to split input")?;
+
+    let mut ranges: Vec<(u64, u64)> = parse_input(
+        LineSplitter,
+        ParseTuple2(ParseFromStr, ParseFromStr, "-"),
+        sec1,
+    )
+    .context("failed to parse input")?;
+
+    let ids: Vec<u64> =
+        parse_input(LineSplitter, ParseFromStr, sec2).context("failed to parse input")?;
+
+    ranges.sort();
+    println!("{ranges:?}");
+    println!("{ids:?}");
+
+    let mut out = 0;
+    for id in ids {
+        for (start, end) in &ranges {
+            if id >= *start && id <= *end {
+                out += 1;
+                break;
+            }
+        }
+    }
+
+    Ok(Some(out))
+}
+
+pub fn part_two(_input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::Error> {
+    Ok(None)
+}
+
+#[cfg(test)]
+mod tests_day_5 {
+    use super::*;
+
+    #[test]
+    fn test_part_one() -> anyhow::Result<()> {
+        let expected = Some(3);
+        let input = &advent_of_code::template::read_file_part("examples", DAY, 1);
+        assert!(expected.is_none() || !input.is_empty(), "example 1 empty!");
+        let result = part_one(input, RunType::Example)?;
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two() -> anyhow::Result<()> {
+        let expected = None;
+        let input = &advent_of_code::template::read_file_part("examples", DAY, 2);
+        assert!(expected.is_none() || !input.is_empty(), "example 2 empty!");
+        let result = part_two(input, RunType::Example)?;
+        assert_eq!(result, expected);
+        Ok(())
+    }
+}
