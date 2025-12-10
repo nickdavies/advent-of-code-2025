5c43a2ccdf7f55725095e578c3d65d95e02e7bdf
Nick Davies <git@nicolasdavies.com.au>
2025-12-09 23:56:13 -0700

    Update the same logic to run for part2
    Mostly parsing and copy/paste the main function. This in theory works
    but OOMs so I will need to find a faster solution

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -11,11 +11,11 @@ use std::collections::BinaryHeap;
 use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
-struct Buttons {
+struct LightButtons {
     buttons: Vec<bool>,
 }
 
-impl Buttons {
+impl LightButtons {
     fn apply_to(&self, mut other: Vec<bool>) -> Vec<bool> {
         for (idx, toggle) in self.buttons.iter().enumerate() {
             if *toggle {
@@ -30,34 +30,50 @@ impl Buttons {
     }
 }
 
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
+struct JoltageButtons {
+    buttons: Vec<usize>,
+}
+
+impl JoltageButtons {
+    fn apply_to(&self, mut other: Vec<usize>) -> Vec<usize> {
+        for (idx, value) in self.buttons.iter().enumerate() {
+            other[idx] += value;
+        }
+        other
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Machine {
     lights: Vec<bool>,
-    buttons: Vec<Buttons>,
-    joltages: Vec<Vec<usize>>,
+    light_buttons: Vec<LightButtons>,
+
+    joltages: Vec<usize>,
+    joltage_buttons: Vec<JoltageButtons>,
 }
 
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
-struct Item {
+struct Item<T> {
     cost: i64,
-    lights: Vec<bool>,
+    target: T,
     path: Vec<usize>,
 }
 
 impl Machine {
-    fn press_combinations(&self, max_cost: usize) -> Result<Item> {
+    fn press_light_combinations(&self, max_cost: usize) -> Result<Item<Vec<bool>>> {
         let mut next = BinaryHeap::new();
         let mut seen = BTreeSet::new();
 
         next.push(Item {
             cost: 0,
-            lights: vec![false; self.lights.len()],
+            target: vec![false; self.lights.len()],
             path: Vec::new(),
         });
 
         while let Some(item) = next.pop() {
-            seen.insert(item.lights.clone());
-            if item.lights == self.lights {
+            seen.insert(item.target.clone());
+            if item.target == self.lights {
                 return Ok(item);
             }
 
@@ -68,17 +84,75 @@ impl Machine {
             let delta: Vec<usize> = self
                 .lights
                 .iter()
-                .zip(item.lights.iter())
+                .zip(item.target.iter())
                 .enumerate()
                 .filter_map(|(idx, (a, b))| if a != b { Some(idx) } else { None })
                 .collect();
 
-            for (i, buttons) in self.buttons.iter().enumerate() {
+            for (i, buttons) in self.light_buttons.iter().enumerate() {
                 if delta.iter().any(|idx| buttons.useful_for(*idx)) {
                     let mut new = item.clone();
                     new.cost -= 1;
-                    new.lights = buttons.apply_to(new.lights);
-                    if seen.contains(&new.lights) {
+                    new.target = buttons.apply_to(new.target);
+                    if seen.contains(&new.target) {
+                        continue;
+                    }
+                    new.path.push(i);
+
+                    next.push(new);
+                }
+            }
+        }
+
+        Err(anyhow!("ran out of options!"))
+    }
+
+    fn press_joltage_combinations(&self, max_cost: usize) -> Result<Item<Vec<usize>>> {
+        let mut next = BinaryHeap::new();
+        let mut seen = BTreeSet::new();
+
+        next.push(Item {
+            cost: 0,
+            target: vec![0; self.joltages.len()],
+            path: Vec::new(),
+        });
+
+        while let Some(item) = next.pop() {
+            seen.insert(item.target.clone());
+            if item.target == self.joltages {
+                return Ok(item);
+            }
+
+            if item.cost < -(max_cost as i64) {
+                return Err(anyhow!("reached max cost!"));
+            }
+
+            let mut delta = Vec::new();
+            let mut at_target = Vec::new();
+            let mut invalid = false;
+            for (idx, (a, b)) in self.joltages.iter().zip(item.target.iter()).enumerate() {
+                if a == b {
+                    at_target.push(idx);
+                } else if b < a {
+                    delta.push(idx);
+                } else if b > a {
+                    invalid = true;
+                    break;
+                }
+            }
+            if invalid {
+                continue;
+            }
+
+            for (i, buttons) in self.joltage_buttons.iter().enumerate() {
+                let any_helpful = delta.iter().any(|idx| buttons.buttons[*idx] != 0);
+                let any_harmful = at_target.iter().any(|idx| buttons.buttons[*idx] != 0);
+
+                if any_helpful && !any_harmful {
+                    let mut new = item.clone();
+                    new.cost -= 1;
+                    new.target = buttons.apply_to(new.target);
+                    if seen.contains(&new.target) {
                         continue;
                     }
                     new.path.push(i);
@@ -95,17 +169,26 @@ impl Machine {
 impl std::str::FromStr for Machine {
     type Err = anyhow::Error;
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let mut iter = input.split(" ");
+        let (first, rest) = input
+            .split_once(" ")
+            .context("expected to be able to split off lights")?;
+        let (rest, last) = rest
+            .rsplit_once(" ")
+            .context("expected to be able to split off joltages")?;
 
-        let first: &str = iter.next().context("expected to find lights for line")?;
         let raw_lights: Vec<char> = StripPrefix(StripSuffix(Chars(Identity), "]"), "[")
             .parse_section(first)
             .context("failed to parse lights")?;
         let lights: Vec<bool> = raw_lights.into_iter().map(|c| c == '#').collect();
 
-        let mut buttons = Vec::new();
-        let mut joltages = Vec::new();
-        for section in iter {
+        let joltages: Vec<usize> =
+            StripPrefix(StripSuffix(SplitDelim(ParseFromStr, ","), "}"), "{")
+                .parse_section(last)
+                .context("failed to parse joltages")?;
+
+        let mut light_buttons = Vec::new();
+        let mut joltage_buttons = Vec::new();
+        for section in rest.split(" ") {
             match section
                 .chars()
                 .next()
@@ -117,21 +200,21 @@ impl std::str::FromStr for Machine {
                         .parse_section(section)
                         .context("failed to parse buttons section")?;
 
-                    let mut new_buttons = vec![false; lights.len()];
-                    for button in raw_buttons {
-                        new_buttons[button] = true;
+                    let mut new_light_buttons = vec![false; lights.len()];
+                    let mut new_joltage_buttons = vec![0; joltages.len()];
+                    for button in raw_buttons.iter() {
+                        new_light_buttons[*button] = true;
+                        new_joltage_buttons[*button] = 1;
                     }
 
-                    buttons.push(Buttons {
-                        buttons: new_buttons,
+                    light_buttons.push(LightButtons {
+                        buttons: new_light_buttons,
                     });
-                }
-                '{' => {
-                    let parser = StripPrefix(StripSuffix(SplitDelim(ParseFromStr, ","), "}"), "{");
-                    let raw_joltage: Vec<usize> = parser
-                        .parse_section(section)
-                        .context("failed to parse joltage section")?;
-                    joltages.push(raw_joltage);
+
+                    new_joltage_buttons.resize(joltages.len(), 0);
+                    joltage_buttons.push(JoltageButtons {
+                        buttons: new_joltage_buttons,
+                    });
                 }
                 _ => {
                     return Err(anyhow!("got unexpected section: {}", section));
@@ -141,8 +224,9 @@ impl std::str::FromStr for Machine {
 
         Ok(Machine {
             lights,
-            buttons,
             joltages,
+            light_buttons,
+            joltage_buttons,
         })
     }
 }
@@ -156,7 +240,7 @@ pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<i64>, anyhow::
     let mut out = 0;
     for machine in machines {
         let combo = machine
-            .press_combinations(20)
+            .press_light_combinations(20)
             .context("failed to find combo")?;
 
         out += -combo.cost;
@@ -166,8 +250,22 @@ pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<i64>, anyhow::
     Ok(Some(out))
 }
 
-pub fn part_two(_input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::Error> {
-    Ok(None)
+pub fn part_two(input: &str, _run_type: RunType) -> Result<Option<i64>, anyhow::Error> {
+    let machines: Vec<Machine> =
+        parse_input(LineSplitter, ParseFromStr, input).context("failed to parse input")?;
+
+    let mut out = 0;
+    for machine in machines {
+        println!("machine={machine:?}");
+        let combo = machine
+            .press_joltage_combinations(20)
+            .context("failed to find combo")?;
+
+        out += -combo.cost;
+        println!("combo={combo:?}");
+    }
+
+    Ok(Some(out))
 }
 
 #[cfg(test)]
