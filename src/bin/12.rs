94c989322043e6761024e913f7309e3c8bb5a469
Nick Davies <git@nicolasdavies.com.au>
2025-12-12 18:15:21 -0700

    Cleanup day 12 and make example my real input

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -9,11 +9,6 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap};
 
 use anyhow::{Context, Result, anyhow};
 
-fn rotate_map<T: Clone>(map: &Map<T>) -> Map<T> {
-    let width = map.width().unwrap();
-    map.transform(|loc, _| map.get(&Location(width - loc.1 - 1, loc.0)).clone())
-}
-
 pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::Error> {
     let (raw_presents, ranges) = input
         .rsplit_once("\n\n")
@@ -32,33 +27,37 @@ pub fn part_one(input: &str, _run_type: RunType) -> Result<Option<u32>, anyhow::
 
     let mut presents = Vec::new();
     for section in raw_presents.split("\n\n") {
-        let (id, char_grid) = section
+        let (_, char_grid) = section
             .split_once(":\n")
             .context("failed to split present")?;
-        let north: Map<bool> = Map::parse(char_grid, |c| Ok::<bool, anyhow::Error>(c == '#'))
-            .context("Failed to convert present to map")?;
-
-        println!("id={id}");
-        north.print(|v, _| if *v { '#' } else { '.' });
-
+        let total_area = char_grid.len();
         let count = char_grid.chars().filter(|c| *c == '#').count();
-        presents.push(count);
+        presents.push((count, total_area));
     }
 
-    println!("{presents:?}");
-    println!("{ranges:?}");
     let mut out = 0;
     for ((x, y), counts) in ranges {
         let total = x * y;
-        let mut used = 0;
-        for (count, present_count) in counts.iter().zip(presents.iter()) {
-            used += count * present_count;
-            if used > total {
+        let mut min_needed = 0;
+        let mut max_needed = 0;
+        for (count, (present_count, total_area)) in counts.iter().zip(presents.iter()) {
+            min_needed += count * present_count;
+            max_needed += total_area;
+            if min_needed > total {
                 break;
             }
         }
-        if used < total {
+        // If the total present area is > total available then it doesn't matter
+        // how we arrange things it won't fit.
+        if min_needed > total {
+            continue;
+        // If we place gifts without any overlap at all will it fit?
+        } else if max_needed < total {
             out += 1;
+        } else {
+            return Err(anyhow!(
+                "found ambigious range: total={total}, min_needed={min_needed}, max_needed={max_needed}, {counts:?}"
+            ));
         }
     }
 
@@ -75,7 +74,7 @@ mod tests_day_12 {
 
     #[test]
     fn test_part_one() -> anyhow::Result<()> {
-        let expected = Some(2);
+        let expected = Some(528);
         let input = &advent_of_code::template::read_file_part("examples", DAY, 1);
         assert!(expected.is_none() || !input.is_empty(), "example 1 empty!");
         let result = part_one(input, RunType::Example)?;
@@ -91,40 +90,5 @@ mod tests_day_12 {
         let result = part_two(input, RunType::Example)?;
         assert_eq!(result, expected);
         Ok(())
-    }
-
-    #[test]
-    fn test_rotate() -> anyhow::Result<()> {
-        let north = "abc\nd..\nefg";
-        let east = "eda\nf.b\ng.c";
-        let south = "gfe\n..d\ncba";
-        let west = "c.g\nb.f\nade";
-
-        let map_north: Map<char> = Map::try_from(north)?;
-        let map_east: Map<char> = Map::try_from(east)?;
-        let map_south: Map<char> = Map::try_from(south)?;
-        let map_west: Map<char> = Map::try_from(west)?;
-
-        println!("\nNorth");
-        print_map(&map_north);
-        println!("\nEast");
-        print_map(&map_east);
-        println!("\nSouth");
-        print_map(&map_south);
-        println!("\nwest");
-        print_map(&map_west);
-
-        println!("\nrotated");
-        print_map(&rotate_map(&map_north));
-        assert_eq!(map_east.0, rotate_map(&map_north).0);
-
-        assert_eq!(map_south.0, rotate_map(&map_east).0);
-        assert_eq!(map_west.0, rotate_map(&map_south).0);
-        assert_eq!(map_north.0, rotate_map(&map_west).0);
-        Ok(())
-    }
-
-    fn print_map(map: &Map<char>) {
-        map.print(|v, _| *v);
     }
 }
